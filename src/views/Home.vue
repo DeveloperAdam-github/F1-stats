73b0d69ffbab590710cb2dc1a609b731ae4e4d4d
Adam Powell <adam@ecce.uk>
2022-03-20 20:05:05 +0000

    updated

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,7 +4,66 @@
       <div class="h-84vh w-full flex flex-col px-6 py-2 md:p-6">
         <home-title class="mb-3" />
         <div class="w-full h-full flex flex-col text-white">
-          <div class="h-1/2 w-full last-race">
+          <div
+            v-if="lastRaceResults.Circuit"
+            class="h-1/2 w-full last-race"
+            :style="{
+              'background-image':
+                lastRaceResults.Circuit.circuitId === 'bahrain'
+                  ? this.bahrain
+                  : lastRaceResults.Circuit.circuitId === 'imola'
+                  ? this.imola
+                  : lastRaceResults.Circuit.circuitId === 'portimao'
+                  ? this.portimao
+                  : lastRaceResults.Circuit.circuitId === 'catalunya'
+                  ? this.catalunya
+                  : lastRaceResults.Circuit.circuitId === 'monaco'
+                  ? this.monaco
+                  : lastRaceResults.Circuit.circuitId === 'BAK'
+                  ? this.BAK
+                  : lastRaceResults.Circuit.circuitId === 'ricard'
+                  ? this.ricard
+                  : lastRaceResults.Circuit.circuitId === 'red_bull_ring'
+                  ? this.red_bull_ring
+                  : lastRaceResults.Circuit.circuitId === 'silverstone'
+                  ? this.silverstone
+                  : lastRaceResults.Circuit.circuitId === 'hungaroring'
+                  ? this.hungaroring
+                  : lastRaceResults.Circuit.circuitId === 'spa'
+                  ? this.spa
+                  : lastRaceResults.Circuit.circuitId === 'zandvoort'
+                  ? this.zandvoort
+                  : lastRaceResults.Circuit.circuitId === 'monza'
+                  ? this.monza
+                  : lastRaceResults.Circuit.circuitId === 'sochi'
+                  ? this.sochi
+                  : lastRaceResults.Circuit.circuitId === 'istanbul'
+                  ? this.istanbul
+                  : lastRaceResults.Circuit.circuitId === 'americas'
+                  ? this.americas
+                  : lastRaceResults.Circuit.circuitId === 'rodriguez'
+                  ? this.rodriguez
+                  : lastRaceResults.Circuit.circuitId === 'interlagos'
+                  ? this.interlagos
+                  : lastRaceResults.Circuit.circuitId === 'losail'
+                  ? this.losail
+                  : lastRaceResults.Circuit.circuitId === 'jeddah'
+                  ? this.jeddah
+                  : lastRaceResults.Circuit.circuitId === 'yas_marina'
+                  ? this.yas_marina
+                  : lastRaceResults.Circuit.circuitId === 'albert_park'
+                  ? this.albert_park
+                  : lastRaceResults.Circuit.circuitId === 'miami'
+                  ? this.miami
+                  : lastRaceResults.Circuit.circuitId === 'villeneuve'
+                  ? this.villeneuve
+                  : lastRaceResults.Circuit.circuitId === 'marina_bay'
+                  ? this.marina
+                  : lastRaceResults.Circuit.circuitId === 'suzuka'
+                  ? this.suzuka
+                  : '',
+            }"
+          >
             <div class="w-full p-2 lg:py-3 home">
               <h2 class="font-headline">Last Race</h2>
               <p
@@ -148,7 +207,66 @@
               </div>
             </div>
           </div>
-          <div class="h-1/2 w-full upcoming-race">
+          <div
+            class="h-1/2 w-full upcoming-race"
+            v-if="upcomingRace.Circuit"
+            :style="{
+              'background-image':
+                upcomingRace.Circuit.circuitId === 'bahrain'
+                  ? this.bahrain
+                  : upcomingRace.Circuit.circuitId === 'imola'
+                  ? this.imola
+                  : upcomingRace.Circuit.circuitId === 'portimao'
+                  ? this.portimao
+                  : upcomingRace.Circuit.circuitId === 'catalunya'
+                  ? this.catalunya
+                  : upcomingRace.Circuit.circuitId === 'monaco'
+                  ? this.monaco
+                  : upcomingRace.Circuit.circuitId === 'BAK'
+                  ? this.BAK
+                  : upcomingRace.Circuit.circuitId === 'ricard'
+                  ? this.ricard
+                  : upcomingRace.Circuit.circuitId === 'red_bull_ring'
+                  ? this.red_bull_ring
+                  : upcomingRace.Circuit.circuitId === 'silverstone'
+                  ? this.silverstone
+                  : upcomingRace.Circuit.circuitId === 'hungaroring'
+                  ? this.hungaroring
+                  : upcomingRace.Circuit.circuitId === 'spa'
+                  ? this.spa
+                  : upcomingRace.Circuit.circuitId === 'zandvoort'
+                  ? this.zandvoort
+                  : upcomingRace.Circuit.circuitId === 'monza'
+                  ? this.monza
+                  : upcomingRace.Circuit.circuitId === 'sochi'
+                  ? this.sochi
+                  : upcomingRace.Circuit.circuitId === 'istanbul'
+                  ? this.istanbul
+                  : upcomingRace.Circuit.circuitId === 'americas'
+                  ? this.americas
+                  : upcomingRace.Circuit.circuitId === 'rodriguez'
+                  ? this.rodriguez
+                  : upcomingRace.Circuit.circuitId === 'interlagos'
+                  ? this.interlagos
+                  : upcomingRace.Circuit.circuitId === 'losail'
+                  ? this.losail
+                  : upcomingRace.Circuit.circuitId === 'jeddah'
+                  ? this.jeddah
+                  : upcomingRace.Circuit.circuitId === 'yas_marina'
+                  ? this.yas_marina
+                  : upcomingRace.Circuit.circuitId === 'albert_park'
+                  ? this.albert_park
+                  : upcomingRace.Circuit.circuitId === 'miami'
+                  ? this.miami
+                  : upcomingRace.Circuit.circuitId === 'villeneuve'
+                  ? this.villeneuve
+                  : upcomingRace.Circuit.circuitId === 'marina_bay'
+                  ? this.marina
+                  : upcomingRace.Circuit.circuitId === 'suzuka'
+                  ? this.suzuka
+                  : '',
+            }"
+          >
             <div class="w-full p-2 home lg:py-3">
               <h2 class="font-headline">Upcoming Race</h2>
               <p
@@ -295,6 +413,32 @@
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue';
 import HomeTitle from '../components/HomeTitle.vue';
+import albert from '../assets/Circuits/albert.jpeg';
+import americas from '../assets/Circuits/americas.jpeg';
+import bahrain from '../assets/Circuits/bahrain.jpeg';
+import baku from '../assets/Circuits/baku.jpeg';
+import catalunya from '../assets/Circuits/catalunya.jpeg';
+import circuit_gilles from '../assets/Circuits/circuit_gilles.jpeg';
+import hungaroring from '../assets/Circuits/hungaroring.jpeg';
+import imola from '../assets/Circuits/imola.jpeg';
+import interlagos from '../assets/Circuits/interlagos.jpeg';
+import istanbul from '../assets/Circuits/istanbul.jpeg';
+import jeddah from '../assets/Circuits/jeddah.jpeg';
+import losail from '../assets/Circuits/losail.jpeg';
+import marina from '../assets/Circuits/marina.jpeg';
+import miami from '../assets/Circuits/miami.jpeg';
+import monaco from '../assets/Circuits/monaco.jpeg';
+import monza from '../assets/Circuits/monza.jpeg';
+import portimao from '../assets/Circuits/portimao.jpeg';
+import red_bull_ring from '../assets/Circuits/red_bull_ring.jpeg';
+import ricard from '../assets/Circuits/ricard.jpeg';
+import rodriguez from '../assets/Circuits/rodriguez.jpeg';
+import silverstone from '../assets/Circuits/silverstone.jpeg';
+import sochi from '../assets/Circuits/sochi.jpeg';
+import spa from '../assets/Circuits/spa.jpeg';
+import suzuka from '../assets/Circuits/suzuka.jpeg';
+import yas_marina from '../assets/Circuits/yas_marina.jpeg';
+import zandvoort from '../assets/Circuits/zandvoort.jpeg';
 import axios from 'axios';
 
 export default {
@@ -314,6 +458,32 @@ export default {
       alpine: '#0090FF',
       aston_martin: '#006F62',
       williams: '#005AFF',
+      bahrain: `linear-gradient(to bottom right, rgba(1,1,1,0.25), rgba(1,1,1,0.45)), url(${bahrain})`,
+      imola: `linear-gradient(to bottom right, rgba(1,1,1,0.25), rgba(1,1,1,0.45)),url(${imola})`,
+      portimao: `linear-gradient(to bottom right, rgba(1,1,1,0.25), rgba(1,1,1,0.45)),url(${portimao})`,
+      catalunya: `linear-gradient(to bottom right, rgba(1,1,1,0.25), rgba(1,1,1,0.45)),url(${catalunya})`,
+      monaco: `linear-gradient(to bottom right, rgba(1,1,1,0.25), rgba(1,1,1,0.45)),url(${monaco})`,
+      BAK: `linear-gradient(to bottom right, rgba(1,1,1,0.25), rgba(1,1,1,0.45)),url(${baku})`,
+      ricard: `linear-gradient(to bottom right, rgba(1,1,1,0.25), rgba(1,1,1,0.45)),url(${ricard})`,
+      red_bull_ring: `linear-gradient(to bottom right, rgba(1,1,1,0.25), rgba(1,1,1,0.45)),url(${red_bull_ring})`,
+      silverstone: `linear-gradient(to bottom right, rgba(1,1,1,0.25), rgba(1,1,1,0.45)),url(${silverstone})`,
+      hungaroring: `linear-gradient(to bottom right, rgba(1,1,1,0.25), rgba(1,1,1,0.45)),url(${hungaroring})`,
+      spa: `linear-gradient(to bottom right, rgba(1,1,1,0.25), rgba(1,1,1,0.45)),url(${spa})`,
+      zandvoort: `linear-gradient(to bottom right, rgba(1,1,1,0.25), rgba(1,1,1,0.45)),url(${zandvoort})`,
+      monza: `linear-gradient(to bottom right, rgba(1,1,1,0.25), rgba(1,1,1,0.45)),url(${monza})`,
+      sochi: `linear-gradient(to bottom right, rgba(1,1,1,0.25), rgba(1,1,1,0.45)),url(${sochi})`,
+      istanbul: `linear-gradient(to bottom right, rgba(1,1,1,0.25), rgba(1,1,1,0.45)),url(${istanbul})`,
+      americas: `linear-gradient(to bottom right, rgba(1,1,1,0.25), rgba(1,1,1,0.45)),url(${americas})`,
+      rodriguez: `linear-gradient(to bottom right, rgba(1,1,1,0.25), rgba(1,1,1,0.45)),url(${rodriguez})`,
+      interlagos: `linear-gradient(to bottom right, rgba(1,1,1,0.25), rgba(1,1,1,0.45)),url(${interlagos})`,
+      losail: `linear-gradient(to bottom right, rgba(1,1,1,0.25), rgba(1,1,1,0.45)),url(${losail})`,
+      jeddah: `linear-gradient(to bottom right, rgba(1,1,1,0.25), rgba(1,1,1,0.45)),url(${jeddah})`,
+      yas_marina: `linear-gradient(to bottom right, rgba(1,1,1,0.25), rgba(1,1,1,0.45)),url(${yas_marina})`,
+      albert_park: `linear-gradient(to bottom right, rgba(1,1,1,0.25), rgba(1,1,1,0.45)),url(${albert})`,
+      miami: `linear-gradient(to bottom right, rgba(1,1,1,0.25), rgba(1,1,1,0.45)),url(${miami})`,
+      villeneuve: `linear-gradient(to bottom right, rgba(1,1,1,0.25), rgba(1,1,1,0.45)),url(${circuit_gilles})`,
+      suzuka: `linear-gradient(to bottom right, rgba(1,1,1,0.25), rgba(1,1,1,0.45)),url(${suzuka})`,
+      marina: `linear-gradient(to bottom right, rgba(1,1,1,0.25), rgba(1,1,1,0.45)),url(${marina})`,
     };
   },
   components: {
@@ -340,7 +510,7 @@ export default {
           const todaysDate = Math.floor(new Date().getTime() / 1000.0);
           const raceDate = Math.floor(
             new Date(response.data.MRData.RaceTable.Races[0].date).getTime() /
-              1000.0
+              1000
           );
           const calculatedTime = raceDate - todaysDate;
           this.raceDate = calculatedTime;
